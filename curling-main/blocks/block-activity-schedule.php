6e3ac68b82fa8aad00bdbd9246a849aee3ba6f86
bryce.sayers-kwan <bryce.sayers-kwan@cossette.com>
2019-09-18 09:50:39 -0700

    Edited activity schedule block styles

--- curling-main/blocks/block-activity-schedule.php
+++ curling-main/blocks/block-activity-schedule.php
@@ -12,28 +12,30 @@
 ?>
 
 <section class="activity-schedule-block">
-  <h3><?php echo $activity_title; ?></h3>
-  <p class="activity-description"><?php echo $activity_description; ?></p>
-  <div class="schedule-table">
-    <div class="table-title-row">
-      <div><h3>DATE</h3></div>
-      <div><h3>TIME</h3></div>
-      <div><h3>PARTICIPANTS</h3></div>
-    </div>
-    <?php if ( have_rows( 'activity_details', $post_id) ) {
-      while ( the_repeater_field( 'activity_details', $post_id ) ) {
-        $date = get_sub_field( 'date' );
-        $time = get_sub_field( 'time' );
-        $participants = get_sub_field( 'participants' ); ?>
-
-        <div class="activity-row">
-          <div><?php echo $date ?></div>
-          <div><?php echo $time ?></div>
-          <!-- Fix participants, check if it should just be a text field -->
-          <div><?php echo $participants[0]->post_title . " vs. " . $participants[1]->post_title; ?></div>
-        </div>
-
+  <div class="content-container">
+    <h3><?php echo $activity_title; ?></h3>
+    <p class="activity-description"><?php echo $activity_description; ?></p>
+    <div class="schedule-table">
+      <div class="table-title-row">
+        <div><h3>DATE</h3></div>
+        <div><h3>TIME</h3></div>
+        <div><h3>PARTICIPANTS</h3></div>
+      </div>
+      <?php if ( have_rows( 'activity_details', $post_id) ) {
+        while ( the_repeater_field( 'activity_details', $post_id ) ) {
+          $date = get_sub_field( 'date' );
+          $time = get_sub_field( 'time' );
+          $participants = get_sub_field( 'participants' ); ?>
+  
+          <div class="activity-row">
+            <div><p><?php echo $date ?></p></div>
+            <div><p><?php echo $time ?></p></div>
+            <!-- Fix participants, check if it should just be a text field -->
+            <div><p><?php echo $participants[0]->post_title . " vs. " . $participants[1]->post_title; ?></p></div>
+          </div>
+  
+        <?php } ?>
       <?php } ?>
-    <?php } ?>
+    </div>
   </div>
 </section>
