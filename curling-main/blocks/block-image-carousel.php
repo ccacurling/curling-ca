91c23ce310decc49fd5965ac5fbaec10a9ada3ad
Charles Almoite <charles.almoite@cossette.com>
2019-09-20 15:59:20 -0700

    Fix some styling

--- curling-main/blocks/block-image-carousel.php
+++ curling-main/blocks/block-image-carousel.php
@@ -21,7 +21,7 @@ $carousel_masterslider_alias = get_field( 'image_carousel_master_slider_alias' )
 
 <section class="block-image-carousel <?php echo $carousel_background_colour === 'gray' ? 'image-carousel-gray' : ''; ?> image-carousel-<?php echo $carousel_type; ?> js-image-carousel">
   <div class="image-carousel-container">
-    <div class="image-carousel-sub-container">
+    <div class="image-carousel-sub-container <?php echo !$carousel_top_body ? 'image-carousel-nobody' : ''; ?>">
       <h3 class="image-carousel-title"><?php echo $carousel_title; ?></h3>
 
       <?php
