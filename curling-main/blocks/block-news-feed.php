34daa9309eb3165947273a41d492066aa90a6dea
Charles Almoite <charles.almoite@cossette.com>
2019-10-02 15:36:16 -0700

    Fix News Promo; Remove Header

--- curling-main/blocks/block-news-feed.php
+++ curling-main/blocks/block-news-feed.php
@@ -7,7 +7,6 @@
 ?>
 
 <?php
-  $news_feed_title = get_field( 'news_feed_title' );
   $news_feed_category = get_field( 'news_feed_category' );
 
   $category = "";
@@ -27,8 +26,5 @@
 ?>
 
 <section class="block-news-feed js-news-feed" data-category="<?php echo $category; ?>">
-  <div class="news-feed-title-container">
-    <h3><?php echo $news_feed_title; ?></h3>
-  </div>
  <div class="js-news-feed-items"></div>
 </section>
