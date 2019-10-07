415ffbeac06ba057f711fe4e32b9a1eeb33b3fa2
Charles Almoite <charles.almoite@cossette.com>
2019-10-07 09:57:53 -0700

    Fix styling

--- curling-main/blocks/block-jobs.php
+++ curling-main/blocks/block-jobs.php
@@ -14,43 +14,44 @@
   $job_ad = get_field( 'jobs_ad_space' );
 ?>
 
-<section class="block-jobs">
-  <?php
-    if ($jobs_include_ad) {
-  ?>
-    <div class="jobs-ad">
-      <div class="jobs-ad-container">
-        <?php echo $job_ad; ?>
+<section class="block-jobs content content-full-wrapper">
+  <div class="jobs-container">
+    <?php
+      if ($jobs_include_ad) {
+    ?>
+      <div class="jobs-ad">
+        <div class="jobs-ad-container">
+          <?php echo $job_ad; ?>
+        </div>
       </div>
-    </div>
-  <?php
-    }
-  ?>
-  <?php
-    foreach ($jobs as $key => $job) {
-      $title = $job->post_title;
-      $date = date( 'F j, Y', strtotime($job->post_date) );
-      $location = get_field( 'job_location', $job );
-      $link = get_field( 'job_link' , $job );
-  ?>
-    <div class="jobs-item">
-      <div class="jobs-item-container">
-        <h4 class="job-item-date"><?php echo $date; ?></h4>
-        <h3 class="job-item-title"><?php echo $title; ?></h3>
-        <h3 class="job-item-location"><?php echo $location; ?></h3>
-        <?php
-          if ($link) {
-        ?>
-          <div class="job-link-container">
-            <a class="job-item-link btn-link arrow-right-item arrow-right-large-red" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><h4 class="btn-link-text red"><?php echo $link['title']; ?></h4></a>
-          </div>
-        <?php
-          }
-        ?>
+    <?php
+      }
+    ?>
+    <?php
+      foreach ($jobs as $key => $job) {
+        $title = $job->post_title;
+        $date = date( 'F j, Y', strtotime($job->post_date) );
+        $location = get_field( 'job_location', $job );
+        $link = get_field( 'job_link' , $job );
+    ?>
+      <div class="jobs-item">
+        <div class="jobs-item-container">
+          <h4 class="job-item-date"><?php echo $date; ?></h4>
+          <h3 class="job-item-title"><?php echo $title; ?></h3>
+          <h3 class="job-item-location"><?php echo $location; ?></h3>
+          <?php
+            if ($link) {
+          ?>
+            <div class="job-link-container">
+              <a class="job-item-link btn-link arrow-right-item arrow-right-large-red" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><h4 class="btn-link-text red"><?php echo $link['title']; ?></h4></a>
+            </div>
+          <?php
+            }
+          ?>
+        </div>
       </div>
-    </div>
-  <?php
-    }
-  ?>
-
+    <?php
+      }
+    ?>
+  </div>
 </section>
