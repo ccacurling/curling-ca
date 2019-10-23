1758ff994ecb1b5ddb7007a7cfcd95e7290a4844
Charles Almoite <charles.almoite@cossette.com>
2019-10-23 12:12:30 -0700

    CURLWEB121 Fix video block

--- curling-main/blocks/block-video.php
+++ curling-main/blocks/block-video.php
@@ -5,8 +5,20 @@
  * This is the template that displays the video block.
  */
   $video = get_field('video_embed');
+
+  $url = '';
+  if (preg_match('/src="(.*?)" /', $video, $match) == 1) {
+    $url = $match[1];
+  }
 ?>
 
 <section class="block-video">
-  <?php echo $video; ?>
+  <?php
+    if ($url) {
+  ?>
+    <iframe width="100%" height="100%" src="<?php echo $url; ?>" frameborder="0" allowfullscreen>
+  <?php
+    }
+  ?>
+</iframe>
 </section>
